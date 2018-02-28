07b3ef3afd30a2a3771b4efc2f37cbde74d66aed
Michel Gorter <michelgorter@outlook.com>
2018-02-28 17:42:09 +0100

    test
    Co-Authored-By: Michel Gorter <michelgorter@outlook.com>

--- PacketSamurai - Deluxe/PacketSamurai -Deluxe/src/com/aionemu/packetsamurai/utils/collector/data/npcskills/NpcSkillUseStats.java
+++
@@ -1,8 +0,0 @@
-package com.aionemu.packetsamurai.utils.collector.data.npcskills;
-
-public class NpcSkillUseStats {
-	public int maxHp;
-	public int minHp;
-	public int useCount;
-	public float probability;
-}
--- PacketSamurai - Deluxe/PacketSamurai -Deluxe/src/com/aionemu/packetsamurai/utils/collector/objects/Npc.java
+++
@@ -1,9 +0,0 @@
-package com.aionemu.packetsamurai.utils.collector.objects;
-
-public class Npc extends VisibleObject {
-
-	public Npc(int objectId, int templateId, int staticId) {
-		super(objectId, templateId, staticId);
-	}
-
-}
